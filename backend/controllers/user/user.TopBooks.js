a48f6adabbc8f72255213226a4a043855d37ec16
ItBeeZ <bekezoltanrichard-20604@taszi.hu>
2025-12-13 16:03:00 +0100

    Top lekérések lekérések elkészültek és müködnek

--- backend/controllers/user/user.TopBooks.js
+++ backend/controllers/user/user.TopBooks.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { objectEnumValues } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
 
@@ -6,7 +7,6 @@ export const TopBooks = async (req, res) => {
   try {
     const books = await prisma.berles.findMany({
       select: {
-        id: true,
         konyv: {
           select: {
             id: true,
@@ -22,7 +22,7 @@ export const TopBooks = async (req, res) => {
       },
     });
 
-    const gate_books = books.reduce((acc, book) => {
+    const collect_books = books.reduce((acc, book) => {
       const key = book.konyv.id;
 
       if (acc[key]) {
@@ -44,7 +44,7 @@ export const TopBooks = async (req, res) => {
       return acc;
     }, {});
 
-    const sortedBooks = Object.values(gate_books)
+    const sortedBooks = Object.values(collect_books)
       .sort((a, b) => b.elofordulas - a.elofordulas)
       .slice(0, 10);
 
@@ -72,7 +72,7 @@ export const TopAuthor = async (req, res) => {
       },
     });
 
-    const gate_books = books.reduce((acc, book) => {
+    const collect_books = books.reduce((acc, book) => {
       const key = book.konyv.szerzo.id;
 
       if (acc[key]) {
@@ -89,11 +89,85 @@ export const TopAuthor = async (req, res) => {
       return acc;
     }, {});
 
-    const sortedBooks = Object.values(gate_books)
+    const sortedBooks = Object.values(collect_books)
       .sort((a, b) => b.elofordulas - a.elofordulas)
       .slice(0, 10);
 
     return res.json(sortedBooks);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const TopByStars = async (req, res) => {
+  try {
+    const books = await prisma.konyv.findMany({
+      where: {
+        csillag_ertekeles: { gte: 4.8 },
+      },
+      select: {
+        id: true,
+        cim: true,
+        kep: true,
+        leiras: true,
+        csillag_ertekeles: true,
+        szerzo: { select: { nev: true } },
+        kiado: { select: { nev: true } },
+        kategoria: { select: { nev: true } },
+      },
+      take: 25,
+    });
+
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const TopByCategory = async (req, res) => {
+  try {
+    const books = await prisma.berles.findMany({
+      select: {
+        id: true,
+        konyv: {
+          select: {
+            cim: true,
+            kep: true,
+            leiras: true,
+            szerzo: { select: { nev: true } },
+            kiado: { select: { nev: true } },
+            kategoria: { select: { nev: true, id: true } },
+          },
+        },
+      },
+      orderBy: { id: "asc" },
+    });
+
+    const collect_books = books.reduce((acc, book) => {
+      const key = book.konyv.kategoria.id;
+
+      if (acc[key]) acc[key].elofordulas++;
+      else {
+        acc[key] = {
+          id: key,
+          kategoria: book.konyv.kategoria.nev,
+          cim: book.konyv.cim,
+          kep: book.konyv.kep,
+          leiras: book.konyv.leiras,
+          szerzo: book.konyv.szerzo.nev,
+          kiado: book.konyv.kiado.nev,
+          elofordulas: 1,
+        };
+      }
+
+      return acc;
+    }, {});
+
+    const sortedBooks = Object.values(collect_books)
+      .sort((a, b) => b.elofordulas - a.elofordulas)
+      .slice(0, 10);
+
+    return res.status(200).json(sortedBooks);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
-import { TopBooks, TopAuthor } from "../controllers/user/user.TopBooks.js";
+import {
+  TopBooks,
+  TopAuthor,
+  TopByStars,
+  TopByCategory,
+} from "../controllers/user/user.TopBooks.js";
 
 const userRouter = Router();
 
 userRouter.route("/top-books").get(TopBooks);
 userRouter.route("/top-author").get(TopAuthor);
+userRouter.route("/top-by-stars").get(TopByStars);
+userRouter.route("/top-by-category").get(TopByCategory);
 
 export default userRouter;
