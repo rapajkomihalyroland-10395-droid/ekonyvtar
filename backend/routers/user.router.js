716544d2ebab52bfd4b78f3e7a32259123d33dce
ItBeeZ <bekezoltanrichard-20604@taszi.hu>
2025-12-17 22:39:21 +0100

    Vélemény szűrés előkészülete

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -7,11 +7,17 @@ import {
   TopByCategory,
 } from "../controllers/user/user.TopBooks.js";
 
+import { BookSearching } from "../controllers/user/user.BooksAction.js";
+
 const userRouter = Router();
 
+//TOPLISTÁK
 userRouter.route("/top-books").get(TopBooks);
 userRouter.route("/top-author").get(TopAuthor);
 userRouter.route("/top-by-stars").get(TopByStars);
 userRouter.route("/top-by-category").get(TopByCategory);
+
+//KERESÉS
+userRouter.route("/search/:book_name").get(BookSearching);
 
 export default userRouter;
